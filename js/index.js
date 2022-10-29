1a7049aabb57b23750746915fc0d2ec6c7d1ed1d
franromeroe <franromeroeze@gmail.com>
2022-10-29 16:21:30 -0300

    js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 console.log("hola franz");
+
+var numero = 5;
+console.log(numero);
+
+var miVariable = "hola a todos";
+console.log(miVariable);
+
+var booleano = true;
+console.log(booleano);
+/*
+tipos de variables cadena numerico y booleano
+camelcase FranEsProgramador
+; punto y coma es el fin de una linea y hay que pasar a otra, en js es opcional
+*/
+
+//mi edad
+
+var edad = 29;
+console.log("mi edad es " + edad); // aparece en f12 console
+document.write("mi edad es " + edad); //aparece en la pantalla
+
+
+
+
