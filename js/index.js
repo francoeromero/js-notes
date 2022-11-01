4a6bcd26a1cd56e589f93a3ba5dd9a5e9c2aff6e
franromeroe <franromeroeze@gmail.com>
2022-11-01 16:32:27 -0300

    js

--- js/index.js
+++ js/index.js
@@ -33,4 +33,51 @@ let dos = 2;
 let igual = uno + dos;
 console.log(igual);
 
+//condicionaless booleanos 
+let minumero = 7;
+
+if (minumero == 7){
+    console.log("si es")
+} else {
+    console.log("no es")
+}
+
+//para que diga true o false
+minum = 4;
+
+let resultadoPregunta = minum == 5;
+console.log(resultadoPregunta);
+
+let numeropin = 9;
+if (numeropin == 9){
+    console.log("si");
+} else {
+    console.log("no");
+}
+
+//bucles
+// while es una asignacion para darle un bucle que repita, ejemplo: contar desde 0 hasta el 4.
+
+let contador = 0;
+while(contador < 5) 
+{
+    console.log(contador);
+
+    contador = contador + 1;
+}
+
+//declaras variable inicio
+//limites del bucle final entre el inicio y el final 
+//calculo de cada vuelta
+
+for(let cont = 10; cont > 0; cont = cont - 1){
+    console.log(cont);
+}
+
+// el contador se lo llama i y para sumarle o restarla es i++ o i--
+
+for(let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
 
