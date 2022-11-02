1c528764718d049e36e2e8439881f51c4c779feb
franromeroe <franromeroeze@gmail.com>
2022-11-02 10:56:07 -0300

    js

---
+++ calculator/index.js
@@ -0,0 +1,6 @@
+const txtop1 = document.getElementById("op1");
+const txtoperador = document.getElementById("operador");
+const txtop2 = document.getElementById("op2");
+const btnboton = document.getElementById("boton");
+const presultado = document.getElementById("resultado");
+console.log(txtop1);
