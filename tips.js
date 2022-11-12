108cad9d7b6fe69ca6e7762644dbbf44245a1fbc
franromeroe <franromeroeze@gmail.com>
2022-11-12 11:55:50 -0300

    js

--- tips.js
+++ tips.js
@@ -2,3 +2,8 @@
 
 // JS : transformar una linea en comentario: CTRL + } 
 // HTML: con el punto y nombrar algo se hace un div con su class  ejemplo .content = <div class="content">
+// HTML: li*5>a>span   = <li><a href=""><span></span></a></li>
+                // <li><a href=""><span></span></a></li>
+                // <li><a href=""><span></span></a></li>
+                // <li><a href=""><span></span></a></li>
+                // <li><a href=""><span></span></a></li>
